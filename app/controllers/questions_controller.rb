a38453bb4cb4219e6ffedb59c9ff7ed5ae5439c7
Alexander <sunny.os87@rambler.ru>
2020-04-17 15:31:15 +1000

    add helpers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :find_question, only: %i[show destroy]
-  before_action :find_test, only: %i[index create]
+  before_action :find_test, only: %i[index new create]
 
   rescue_from ActiveRecord::RecordNotFound,
               with: :rescue_with_question_not_found
